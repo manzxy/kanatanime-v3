23419206412da2007f6b40b13de801bc19b86f07
manzxy <141548199+manzxy@users.noreply.github.com>
2026-03-10 23:18:02 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { handle } from '@hono/node-server/vercel'
 import app from '../server/index'
 
 export const config = {
-  runtime: 'nodejs20.x'
+  runtime: 'nodejs'
 }
 
 export default handle(app)
