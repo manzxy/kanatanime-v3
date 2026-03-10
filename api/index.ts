9df40827378b1507bf39411a9e5e7e10e3bf65b5
manzxy <141548199+manzxy@users.noreply.github.com>
2026-03-10 23:33:32 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,12 @@
-import { handle } from '@hono/node-server/vercel'
+import { Hono } from 'hono'
 import app from '../server/index'
 
 export const config = {
   runtime: 'nodejs'
 }
 
-export default handle(app)
+const handler = async (req: Request) => {
+  return app.fetch(req)
+}
+
+export default handler
